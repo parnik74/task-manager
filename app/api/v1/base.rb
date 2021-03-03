a2833c49e45fd6927614bb5dc7a147a4b57f52ac
Semyon Kazantsev <semyon.kazantsev@gmail.com>
2021-03-03 20:44:30 +0300

    v1base fix

--- app/api/v1/base.rb
+++ app/api/v1/base.rb
@@ -9,47 +9,45 @@ module V1
       }
     }.freeze
 
-    # included do
-      format :json
-      prefix :api
-      default_format :json
-      formatter :json,
-      Grape::Formatter::ActiveModelSerializers
+    format :json
+    prefix :api
+    default_format :json
+    formatter :json,
+              Grape::Formatter::ActiveModelSerializers
 
-      version 'v1', using: :header, vendor: API_VENDOR
+    version 'v1', using: :header, vendor: API_VENDOR
 
-      rescue_from ActiveRecord::RecordNotFound do |e|
-        render_error(RESPONSE_CODE[:not_found], I18n.t("errors.#{e.model.to_s.downcase}.not_found"))
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      render_error(RESPONSE_CODE[:not_found], I18n.t("errors.#{e.model.to_s.downcase}.not_found"))
+    end
+
+    rescue_from ApiException do |e|
+      render_error(e.http_status, e.error.message, e.error.debug_info)
+    end
+
+    helpers do
+      def logger
+        Rails.logger
       end
 
-      rescue_from ApiException do |e|
-        render_error(e.http_status, e.error.message, e.error.debug_info)
+      def render_error(code, message, debug_info = '')
+        error!({ meta: { code: code, message: message, debug_info: debug_info } }, code)
       end
 
-      helpers do
-        def logger
-          Rails.logger
-        end
-
-        def render_error(code, message, debug_info = '')
-          error!({ meta: { code: code, message: message, debug_info: debug_info } }, code)
-        end
-
-        def render_success(json, extra_meta = {})
-          { data: json, meta: { code: RESPONSE_CODE[:success], message: 'success' }.merge(extra_meta) }
-        end
-
-        def pagination_dict(object)
-          {
-            current_page: object.current_page,
-            next_page: object.next_page || -1,
-            prev_page: object.prev_page || -1,
-            total_pages: object.total_pages,
-            total_count: object.total_count
-          }
-        end
+      def render_success(json, extra_meta = {})
+        { data: json, meta: { code: RESPONSE_CODE[:success], message: 'success' }.merge(extra_meta) }
       end
-    # end
+
+      def pagination_dict(object)
+        {
+          current_page: object.current_page,
+          next_page: object.next_page || -1,
+          prev_page: object.prev_page || -1,
+          total_pages: object.total_pages,
+          total_count: object.total_count
+        }
+      end
+    end
     mount V1::Projects
     mount V1::Tasks
     mount V1::Users
